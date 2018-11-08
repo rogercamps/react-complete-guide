8c25d458f1f0393c1a91bc7739a05a3f4b78e01c
Roger Camps <rogercamps@me.com>
2018-11-07 22:36:11 -0800

    split app into components

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import './App.css';
-import Person from './Person/Person'
+
+import classes from './App.css';
+import Persons from '../components/Persons/Persons'
+import Cockpit from '../components/Cockpit/Cockpit'
 
 class App extends Component {
   state = {
@@ -45,51 +47,21 @@ class App extends Component {
   }
 
   render() {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    }
-
     let persons = null
 
     if (this.state.showPersons) {
-      persons = (
-        <div>
-          {this.state.persons.map((person, index) => {
-            return <Person
-            click={() => this.deletePersonHandler(index)}
-            name={person.name}
-            age={person.age}
-            key={person.id}
-            changed={(event) => this.nameChangeHandler(event, person.id)} />
-          })}
-        </div> 
-      )
-
-      style.backgroundColor = 'red'
-        }
-
-    const classes = []
-
-    if (this.state.persons.length <= 2) {
-      classes.push('red')
-    }
-
-    if (this.state.persons.length <=1) {
-      classes.push('bold')
+      persons = <Persons 
+            persons={this.state.persons}
+            clicked={this.deletePersonHandler}
+            changed={this.nameChangeHandler} />
     }
   
     return (
-      <div className="App">
-        <h1>Hi! I am a react app!! yessss</h1>
-        <p className={classes.join(' ')}>This is Really working</p>
-        <button 
-          style={style}
-          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+      <div className={classes.App}>
+        <Cockpit 
+          showPersons={this.state.showPersons} 
+          persons={this.state.persons}
+          clicked={this.togglePersonsHandler}/>
         {persons}
       </div>
     );
